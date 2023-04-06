2074cb59c61b68a88b97538b0f6a3a4050ee4634
Nepalcem <mark.emailtest@gmail.com>
2023-04-07 02:22:33 +0300

    fetch + onSearch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './css/style.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more-button'),
+};
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
+
+function onLoadMore() {}
+
+function onSearch(e) {
+  e.preventDefault();
+  const input = e.currentTarget.elements.searchQuery.value;
+  parameters.q = input.trim();
+  fetchImages().then(console.log);
+}
+
+///////
+const BASE_URL = 'https://pixabay.com/api/';
+
+const parameters = {
+  key: '35035540-8bd526b593fab0e390d7ded9d',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  per_page: 40,
+  page: 1,
+};
+
+const fetchImages = async () => {
+  try {
+    const response = await axios.get(BASE_URL, { params: parameters });
+    const imagesArray = response.data;
+    return imagesArray;
+  } catch (error) {
+    return console.log(error.message);
+  }
+};
+
+// fetch(
+//   'https://pixabay.com/api/?key=35035540-8bd526b593fab0e390d7ded9d&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=1'
+// )
